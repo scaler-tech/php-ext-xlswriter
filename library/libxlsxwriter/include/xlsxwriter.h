40df0f4b669a7d6ffae272742ef8cdc5b11df044
Alexander Tebiev <alexander.tebiev@gmail.com>
2025-06-28 16:21:10 +0200

    add libxlsxwriter v1.1.0

---
+++ library/libxlsxwriter/include/xlsxwriter.h
@@ -0,0 +1,25 @@
+/*
+ * libxlsxwriter
+ *
+ * Copyright 2014-2021, John McNamara, jmcnamara@cpan.org. See LICENSE.txt.
+ */
+
+/**
+ * @file xlsxwriter.h
+ *
+ * xlsxwriter - A library for creating Excel XLSX files.
+ *
+ */
+#ifndef __LXW_XLSXWRITER_H__
+#define __LXW_XLSXWRITER_H__
+
+#include "xlsxwriter/workbook.h"
+#include "xlsxwriter/worksheet.h"
+#include "xlsxwriter/format.h"
+#include "xlsxwriter/utility.h"
+
+#define LXW_VERSION "1.1.0"
+#define LXW_VERSION_ID 110
+#define LXW_SOVERSION "2.1"
+
+#endif /* __LXW_XLSXWRITER_H__ */
