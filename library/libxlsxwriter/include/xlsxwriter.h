f2c02d25a65675198d69dfadaeab420db6873719
Alexander Tebiev <326840+beeyev@users.noreply.github.com>
2025-06-30 13:08:34 +0200

    Update README to include fork information and embed dependency libraries (#1)
    Update README to include fork information and embed dependency libraries (#1)

---
+++ library/libxlsxwriter/include/xlsxwriter.h
@@ -0,0 +1,25 @@
+/*
+ * libxlsxwriter
+ *
+ * Copyright 2014-2021, John McNamara, jmcnamara@cpan.org. See LICENSE.txt.
+ */
+
+/**
+ * @file xlsxwriter.h
+ *
+ * xlsxwriter - A library for creating Excel XLSX files.
+ *
+ */
+#ifndef __LXW_XLSXWRITER_H__
+#define __LXW_XLSXWRITER_H__
+
+#include "xlsxwriter/workbook.h"
+#include "xlsxwriter/worksheet.h"
+#include "xlsxwriter/format.h"
+#include "xlsxwriter/utility.h"
+
+#define LXW_VERSION "1.1.3"
+#define LXW_VERSION_ID 113
+#define LXW_SOVERSION "3"
+
+#endif /* __LXW_XLSXWRITER_H__ */
